e75cb3d7ee7e5d29bb2003d4f68d6a453ab74c23
declan-tyson <declan.j.tyson@gmail.com>
2022-02-05 17:43:18 +0000

    added getall routes, parase error: http expected still present

--- api/Server/routes/index.js
+++ api/Server/routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const router = express.Router();
 const http = require('http');
 
 
@@ -7,31 +8,24 @@ const http = require('http');
 // likely not needed here
 // express.request(cors());
 
-const db = require('../db/index.js');
-
-const router = express.Router();
+const sequelize = require('../db/index.js');
 
 
+router.get('/getalll', async (req, res, next) => {
+    await sequelize.query('SELECT * FROM citizen LIMIT 10', { type: sequelize.QueryTypes.SELECT })
+        .then((result) => {
+            res.status(201).send(result);
+        })
+        .catch((err) => {
+            res.status(500).send({
+                message:
+                    err.message || "error while carrying out request"
+            });
+        });
+});
 
-router.get('/getall', async(req, res,next)=>{
-    try{
-        let results = await db.all();
-        res.json(results);
-    } catch(e)
-{
-    console.log(e);
-    res.sendStatus(500);
-}});
 
-router.get('/:id', async(req, res,next)=>{
-    try{
-        let results = await db.one(req.params.id);
-        res.json(results);
-    } catch(e)
-{
-    console.log(e);
-    res.sendStatus(500);
-}});
+
 
 
 module.exports = router;
--- api/Server/server.js
+++ api/Server/server.js
@@ -8,9 +8,17 @@ app.use(cors());
 
 app.use(express.json());
 
+// app.use((req, res, next) => {
+//     console.log("middleware created");
+//     next();
+// })
+
+
+// Insert routes here
+
 app.use('/api/citizens', apiRouter);
-app.listen(process.env.PORT || '3300',()=>{
-    console.log("server is very much working");
+const server = app.listen(process.env.PORT || '3300',()=>{
+    console.log(`Server booted up successfully, on PORT: ${server.address().port}`);
 });
 
 
