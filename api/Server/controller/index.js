d371f04cb6667862a884b976dad6041de60c774f
hazzamak <harry.mclellan1@gmail.com>
2022-02-07 13:36:13 +0000

    Controller added

--- api/Server/controller/index.js
+++ api/Server/controller/index.js
@@ -1,3 +1,15 @@
 'use strict';
 //===================================================
 //controller index must be set up 
+const db = require('../db');
+
+exports.orders_get_all = router.get('/', async (req, res, next) => {
+    try{
+        let results = await db.all();
+        res.json(results);
+        
+    } catch(e){
+        console.log(e);
+        res.sendStatus(500);
+    }
+})
