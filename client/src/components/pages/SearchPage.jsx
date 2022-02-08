19297b1d3ca6ccf3c789abedddf446d43b4a76e0
enarsinick <nxdmedia@googlemail.com>
2022-02-08 19:34:20 +0000

    Updated pagination added, stlying needs to be applied

--- client/src/components/pages/SearchPage.jsx
+++ client/src/components/pages/SearchPage.jsx
@@ -2,6 +2,7 @@ import SearchResultCard from "../SearchResultCard";
 import SearchImage from "../../assets/SearchImage.jpg";
 import LoadingIcon from "../LoadingIcon";
 import {useState, useEffect, useRef} from "react";
+import ReactPaginate from 'react-paginate';
 import axios from "axios";
 import SearchResultWrapper from "../SearchResultWrapper";
 import Pagination from "../Pagination";
@@ -9,8 +10,8 @@ import Pagination from "../Pagination";
 const SearchPage = ({query}) => {
 
     const [loading, setLoading] = useState(false);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [itemsPerPage] = useState(10)
+    // const [currentPage, setCurrentPage] = useState(1);
+    // const [itemsPerPage] = useState(10)
     const [data, setData] = useState([]);
     const isMounted = useRef(false);
 
@@ -36,18 +37,66 @@ const SearchPage = ({query}) => {
         }
     },[query]);
 
-    const indexOfLastItem = currentPage * itemsPerPage;
-    const indexofFirstItem = indexOfLastItem - itemsPerPage;
-    const currentItems = data.slice(indexofFirstItem, indexOfLastItem);
+    // const indexOfLastItem = currentPage * itemsPerPage;
+    // const indexofFirstItem = indexOfLastItem - itemsPerPage;
+    // const currentItems = data.slice(indexofFirstItem, indexOfLastItem);
 
-    const paginate = (pageNumber) => setCurrentPage(pageNumber)
+    // const paginate = (pageNumber) => setCurrentPage(pageNumber)
+
+    // We start with an empty list of items.
+    const [currentItems, setCurrentItems] = useState(null);
+    const [pageCount, setPageCount] = useState(0);
+    // Here we use item offsets; we could also use page offsets
+    // following the API or data you're working with.
+    const [itemOffset, setItemOffset] = useState(0);
+    const [itemsPerPage] = useState(10);
+
+    useEffect(() => {
+        // Fetch items from another resources.
+        const endOffset = itemOffset + itemsPerPage;
+        console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+        setCurrentItems(data.slice(itemOffset, endOffset));
+        setPageCount(Math.ceil(data.length / itemsPerPage));
+    }, [itemOffset, itemsPerPage, data]);
+
+    // Invoke when user click to request another page.
+    const handlePageClick = (event) => {
+        const newOffset = (event.selected * itemsPerPage) % data.length;
+        console.log(
+        `User requested page number ${event.selected}, which is offset ${newOffset}`
+        );
+        setItemOffset(newOffset);
+    };
         
     return ( 
         <div className="mainContentWrapper">
         {loading ? 
             <>
                 <SearchResultWrapper items={currentItems} loading={loading}/>
-                <Pagination itemsPerPage={itemsPerPage} totalItems={data.length} paginate={paginate}/>
+                <div class="paginationWrapper">
+                    <ReactPaginate
+                        nextLabel="next >"
+                        onPageChange={handlePageClick}
+                        pageRangeDisplayed={2}
+                        marginPagesDisplayed={3}
+                        pageCount={pageCount}
+                        previousLabel="< prev"
+                        pageClassName="page-item"
+                        pageLinkClassName="page-link"
+                        previousClassName="page-item"
+                        previousLinkClassName="page-link"
+                        nextClassName="page-item"
+                        nextLinkClassName="page-link"
+                        breakLabel="..."
+                        breakClassName="page-item"
+                        breakLinkClassName="page-link"
+                        containerClassName="pagination"
+                        activeClassName="pagination-active"
+                        renderOnZeroPageCount={null}
+                    />
+                </div>
+                
+                {/* <Pagination itemsPerPage={itemsPerPage} totalItems={data.length} paginate={paginate}/> */}
             </>
             :
             <div className="imageWrapper">
