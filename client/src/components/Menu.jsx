d8f1a89871f658a6e5cb7f1048da22f6ae0b5c82
DanSmith <92935963+DanRaynorSmith@users.noreply.github.com>
2022-02-09 10:33:36 +0000

    Merge conflict actually resloved

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -6,14 +6,7 @@ const Menu = () => {
     const { forenames, surname, dateOfBirth } = useParams();
     return ( 
         <div className="menuWrapper"> 
-<<<<<<< HEAD
-            <NavLink to={`/overview/${forenames}/${surname}/${dateOfBirth}`} className="menuItem" activeClassName="currentPage">Overview</NavLink>
-            <NavLink to={`/biographical/${forenames}/${surname}/${dateOfBirth}`} className="menuItem">Biographical</NavLink>
-            <NavLink to={`/associates/${forenames}/${surname}/${dateOfBirth}`} className="menuItem">Associates</NavLink>
-            <NavLink to={`/bank/name/${forenames}/${surname}/${dateOfBirth}`} className="menuItem">Financial</NavLink>
-            <NavLink to={`/mobile/name/${forenames}/${surname}/${dateOfBirth}`} className="menuItem">Mobile</NavLink>
-            <NavLink to={`/vehicle/name/${forenames}/${surname}/${dateOfBirth}`} className="menuItem">Vehicle</NavLink>
-=======
+
             <NavLink 
                 to={`/overview/${forenames}/${surname}/${dateOfBirth}`} 
                 className={({isActive}) => "menuItem" + (isActive ? " active" : "")}
@@ -27,10 +20,18 @@ const Menu = () => {
                 className={({isActive}) => "menuItem" + (isActive ? " active" : "")}
                 >Associates</NavLink>
             <NavLink 
-                to={`/financial/${forenames}/${surname}/${dateOfBirth}`} 
+                to={`/bank/name/${forenames}/${surname}/${dateOfBirth}`} 
                 className={({isActive}) => "menuItem" + (isActive ? " active" : "")}
                 >Financial</NavLink>
->>>>>>> 45be36a7fb02823921df17bb9f01465371bf743c
+            <NavLink 
+                to={`/mobile/name/${forenames}/${surname}/${dateOfBirth}`} 
+                className={({isActive}) => "menuItem" + (isActive ? " active" : "")}
+                >Financial</NavLink>
+            <NavLink 
+                to={`/vehicle/name/${forenames}/${surname}/${dateOfBirth}`} 
+                className={({isActive}) => "menuItem" + (isActive ? " active" : "")}
+                >Financial</NavLink>
+                
 
         </div>
      );
